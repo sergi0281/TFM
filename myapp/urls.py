156a7aff271788f0abfa94bec6fee4e171afa01d
sergi0281 <sergi.franco@iticbcn.cat>
2025-03-07 19:58:18 +0100

    amb phenapp

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from .views import api_home
+from .views import processar_dades
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('pagina/', views.pagina, name='pagina'),
     path('api/', api_home, name='api_home'),
+    path("api/processar/", processar_dades, name="processar_dades"),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from django.http import HttpResponse
 from django.http import JsonResponse
+## a continuació els que afegeixo per la vista post
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 def home(request):
     return HttpResponse("Hola, això és myapp al iniciar django!")
@@ -15,3 +18,14 @@ def api_home(request):
     data = {"message": "Hola des de Django!"}
     return JsonResponse(data)
 
+@api_view(["POST"])
+
+def processar_dades(request):
+    data = request.data  # Rep les dades enviades per React
+    nom = data.get("nom", "Desconegut")
+    email = data.get("email", "No proporcionat")
+
+    resposta = {
+        "missatge": f"Hola {nom}, hem rebut el teu email {email}!",
+    }
+    return Response(resposta, status=status.HTTP_200_OK)
